97fcb0584543c18cbae72d69f92d74e57a4beac5
Basone01 <basone01@hotmail.com>
2020-04-22 04:09:03 +0700

    🔥 remove TodoListContainer

--- src/features/todo/pages/TodoListPage.tsx
+++ src/features/todo/pages/TodoListPage.tsx
@@ -1,17 +1,30 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useCallback } from "react";
 import { Link } from "react-router-dom";
-import TodoListContainer from "../containers/TodoListContainer";
 import { useTodoContext } from "../todoContext";
+import TodoList from "../components/TodoList";
 
 export default function TodoListPage() {
-  const { fetchAllTodos } = useTodoContext();
+  const { fetchAllTodos, todos, deleteTodo } = useTodoContext();
+
+  const handleDeleteTodo = useCallback(
+    (_id: string) => {
+      const todo = todos.find((todo) => todo._id === _id);
+
+      const confirmed = window.confirm(`Delete ${todo?.title}?`);
+      if (!confirmed) return;
+
+      deleteTodo(_id);
+    },
+    [deleteTodo, todos]
+  );
   useEffect(() => {
     fetchAllTodos();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
   return (
     <>
-      <TodoListContainer />
+      <TodoList todos={todos} onRemove={handleDeleteTodo} />;
       <div>
         <Link to="/todos/add">Create</Link>
       </div>
