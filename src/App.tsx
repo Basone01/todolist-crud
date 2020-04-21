5cbd9426b48e73278e2072573ef2628fb388f423
Basone01 <basone01@hotmail.com>
2020-04-21 22:43:54 +0700

    ✨ register login and todos route

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import LoginPage from "features/auth/LoginPage";
+import { useAuthContext } from "features/auth/authContext";
+import { Switch, Route } from "react-router-dom";
+import ProtectedRoute from "features/auth/ProtectedRoute";
+import TodoListPage from "features/todo/TodoListPage";
 
 function App() {
-  return <div></div>;
+  const { isLoadingFirstAuth } = useAuthContext();
+  if (isLoadingFirstAuth) return <div>LOADING</div>;
+  return (
+    <Switch>
+      <Route path="/todos" component={ProtectedRoute(TodoListPage)} />
+      <Route path="/login" component={LoginPage} />
+    </Switch>
+  );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,14 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
+import { AuthContextProvider } from "features/auth/authContext";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
