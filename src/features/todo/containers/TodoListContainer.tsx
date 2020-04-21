7fb84b944c66162a071f3261a45bf426732e396b
Basone01 <basone01@hotmail.com>
2020-04-22 04:07:04 +0700

    🚧 add confirmation before delete todo

--- src/features/todo/containers/TodoListContainer.tsx
+++ src/features/todo/containers/TodoListContainer.tsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { useTodoContext } from "../todoContext";
 import TodoList from "../components/TodoList";
 
 export default function TodoListContainer() {
   const { todos, deleteTodo } = useTodoContext();
 
-  return <TodoList todos={todos} onRemove={deleteTodo} />;
+  const handleDeleteTodo = useCallback(
+    (_id: string) => {
+      const todo = todos.find((todo) => todo._id === _id);
+
+      const confirmed = window.confirm(`Delete ${todo?.title}?`);
+      if (!confirmed) return;
+
+      deleteTodo(_id);
+    },
+    [deleteTodo, todos]
+  );
+
+  return <TodoList todos={todos} onRemove={handleDeleteTodo} />;
 }
