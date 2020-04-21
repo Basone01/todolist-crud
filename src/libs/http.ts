14e8904f76d14d082f89e7701adba58cc1b6a8ed
Basone01 <basone01@hotmail.com>
2020-04-22 03:02:25 +0700

    🚧 throw axios response error

--- src/libs/http.ts
+++ src/libs/http.ts
@@ -16,24 +16,48 @@ export default class HTTP {
   async get<T = {}>(url: string, config?: IHttpConfig) {
     return axios
       .get<T>(`${this.baseUrl}${url}`, config)
-      .then((res) => res.data);
+      .then((res) => res.data)
+      .catch((error) => {
+        if (error.response) {
+          throw error.response;
+        }
+        throw error;
+      });
   }
 
   async post<T = {}>(url: string, body?: object, config?: IHttpConfig) {
     return axios
       .post<T>(`${this.baseUrl}${url}`, body, config)
-      .then((res) => res.data);
+      .then((res) => res.data)
+      .catch((error) => {
+        if (error.response) {
+          throw error.response;
+        }
+        throw error;
+      });
   }
 
   async put<T = {}>(url: string, body?: object, config?: IHttpConfig) {
     return axios
       .put<T>(`${this.baseUrl}${url}`, body, config)
-      .then((res) => res.data);
+      .then((res) => res.data)
+      .catch((error) => {
+        if (error.response) {
+          throw error.response;
+        }
+        throw error;
+      });
   }
 
   async delete<T = {}>(url: string, config?: IHttpConfig) {
     return axios
       .delete<T>(`${this.baseUrl}${url}`, config)
-      .then((res) => res.data);
+      .then((res) => res.data)
+      .catch((error) => {
+        if (error.response) {
+          throw error.response;
+        }
+        throw error;
+      });
   }
 }
